4fa145e2718ad3df2ee8a665928ba65e1f40d8c6
Tanya Karpenko <karp.tanya22@gmail.com>
2021-09-28 14:47:18 -0500

    main commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import Profile from './components/Profile/Profile';
+import user from './components/Profile/user.json';
+
+import Statistics from './components/Statistic/Statistic';
+import statisticalData from './components/Statistic/statistical-data.json';
+
+
+import FriendList from './components/FriendList/FriendList';
+import friends from './components/FriendList/friends.json';
+
+import TransactionHistory from './components/TransactionHistory/TransactionHistory';
+import transaction from './components/TransactionHistory/transaction.json';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats} />
+      
+      <Statistics title="Upload stats" stats={statisticalData} />
+      <FriendList friends={friends} />
+       <TransactionHistory items={transaction} />
+      
+    </div>
+  );
+}
+
+export default App;
